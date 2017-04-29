6e68c20a6bde0d3d85c5e24606229b1091e9d83b
Alex Masterov <alex.masterow@gmail.com>
2017-04-29 09:37:21 +0300

    Add 'luacheck' Lua config

---
+++ lua/luacheck.lua
@@ -0,0 +1,14 @@
+-- http://luacheck.readthedocs.io/en/stable/config.html#config-options
+codes = true
+color = false
+compat = false
+formatter = "plain"
+global = false
+inline = true
+max_line_length = 120
+module = false
+redefined = true
+self = true
+unused = true
+unused_args = true
+unused_secondaries = true
